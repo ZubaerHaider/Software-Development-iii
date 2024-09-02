bfaa5c3f5e0284d5604c87a7aaa1d1561943a272
Zubaer Haider <zhefti@gmail.com>
2024-09-03 01:06:20 +0600

    New Content added.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import '.*/Navbar.css'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <img src={assets.logo} alt="" />
       
     </div>
   )
